814fcfddc4d9355e290fe23ed3567168cef7df4a
OlehPetryshyn <olegpetryshen@gmail.com>
2023-05-20 16:18:02 +0300

    Homework

---
+++ src/Man.java
@@ -0,0 +1,8 @@
+public class Man extends Person{
+
+    public Man(String firstName, String lastName, int age) {
+        super(firstName, lastName, age );
+    }
+
+
+}
---
+++ src/Woman.java
@@ -0,0 +1,9 @@
+public class Woman extends Person {
+
+    public Woman(String firstName, String lastname, int age) {
+        super(firstName, lastname, age);
+    }
+
+
+
+}
